41b7ed1b32c2f0372f8d53642c9b6f5a4aad4a45
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-16 18:13:46 -0500

    refactor to add orcs and Trolls

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
@@ -12,12 +12,11 @@ from input_handlers import EventHandler
 
 class Engine:
     def __init__(
-        self, entities: Set[Entity],
+        self,
         event_handler: EventHandler,
         game_map: GameMap,
         player: Entity
     ):
-        self.entities = entities
         self.event_handler = event_handler
         self.game_map = game_map
         self.player = player
@@ -46,11 +45,6 @@ class Engine:
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-
-        for entity in self.entities:
-            # Only print entities in the FOV
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
