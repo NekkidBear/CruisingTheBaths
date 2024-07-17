7a253a6efac5bef2e47b513003291a19ab5fd5f1
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-07-17 12:35:35 -0500

    refactoring to allow items and impossible exceptions

--- actions.py
+++ actions.py
@@ -8,7 +8,7 @@ import color
 
 if TYPE_CHECKING:
     from engine import Engine
-    from entity import Actor, Entity
+    from entity import Actor, Entity, Item
 
 
 class Action:
@@ -104,7 +104,7 @@ class MovementAction(ActionWithDirection):
         if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
             # destination is blocked by a tile
             raise exceptions.Impossible("That way is blocked")
-        
+
         if self.engine.game_map.get_blocking_entity_at_location(
                 dest_x, dest_y):
             # destination is blocked by an entity.
@@ -119,3 +119,23 @@ class BumpAction(ActionWithDirection):
             return MeleeAction(self.entity, self.dx, self.dy).perform()
         else:
             return MovementAction(self.entity, self.dx, self.dy).perform()
+
+
+class ItemAction(Action):
+    def __init__(
+        self, entity: Actor, item: Item, target_xy: Optional[Tuple[int, int]] = None
+    ):
+        super().__init__(entity)
+        self.item = item
+        if not target_xy:
+            target_xy = entity.x, entity.y
+        self.target_xy = target_xy
+
+    @property
+    def target_actor(self) -> Optional[Actor]:
+        """Return the actor at this actions destination."""
+        return self.engine.game_map.get_actor_at_location(*self.target_xy)
+
+    def perform(self) -> None:
+        """Invoke the items ability, this action will be given to provide context."""
+        self.item.consumable.activate(self)
